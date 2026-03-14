23b6772dafefb8937dd2375087ffc1892f22b93e
Nikhil Kumar <nikhilkumar5177@gmail.com>
2026-03-14 23:42:09 +0530

    page4 work is done

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function loading() {
     let h5timmer = document.querySelector("#progress h5")
-
     let tl = gsap.timeline();
 
+    function disableScroll() {
+        document.body.style.overflow = "hidden"
+    }
+    
+    function enableScroll() {
+        document.body.style.overflow = "auto"
+    }
+    
+    disableScroll();
     tl.from(".h1loader h1", {
         y: 1500,
-        duration: 1.5
-        // stagger: 0.3,
+        duration: 1.5,
+
+        stagger: 0.3,
     })
+
 
     tl.from("#progress", {
         opacity: 0,
@@ -25,7 +35,7 @@ function loading() {
         }
     })
 
-    tl.from("#page1", {
+    tl.from("#page1, #page2, #page3, #page4, #page5", {
         y: 1600,
         opacity: 0,
         duration: 0.5,
@@ -34,18 +44,17 @@ function loading() {
 
     tl.to("#loader", {
         opacity: 0,
-        display: "none"
+        display: "none",
+        onComplete: enableScroll()
     })
 
     tl.from(".hero h1, .hero h2, .hero h3", {
         y: 100,
         stagger: 0.1,
     })
-
     tl.from("#cursor", {
         opacity: 0
     })
-
     tl.from("#nav-part2", {
         opacity: 0
     })
